2c7aabecc38b0f29596beb7e7af631708b271b77
John McKee <jmckee3@mac.com>
2019-11-25 16:56:19 -0800

    working version with logging

--- ransom-note.go
+++ ransom-note.go
@@ -1,3 +1,29 @@
 package main
 
+import "fmt"
+
+func checkMagazine(magazine []string, note []string) {
+	magMap := make(map[string]int32)
+
+	for _, value := range magazine {
+		magMap[value]++
+	}
+
+	for _, noteValue := range note {
+		if val, ok := magMap[noteValue]; ok {
+			if val > 0 {
+				magMap[noteValue]--
+			} else {
+				fmt.Println("No")
+				return
+			}
+		} else {
+			fmt.Println("No")
+			return
+		}
+	}
+	fmt.Println("Yes")
+	fmt.Println(magMap)
+}
+
 func main() {}
--- spec-ransome-note_test.go
+++ spec-ransome-note_test.go
@@ -2,4 +2,23 @@ package main
 
 import "testing"
 
-func TestRansomNote(t *testing.T) {}
+func TestRansomNote1(t *testing.T) {
+	magazine := []string{"give", "me", "one", "grand", "today", "night"}
+	note := []string{"give", "one", "grand", "today"}
+
+	checkMagazine(magazine, note)
+}
+
+func TestRansomNote2(t *testing.T) {
+	magazine := []string{"two", "times", "three", "is", "not", "four"}
+	note := []string{"two", "times", "two", "is", "four"}
+
+	checkMagazine(magazine, note)
+}
+
+func TestRansomNote3(t *testing.T) {
+	magazine := []string{"ive", "got", "a", "lovely", "bunch", "of", "coconuts"}
+	note := []string{"ive", "got", "some", "coconuts"}
+
+	checkMagazine(magazine, note)
+}
